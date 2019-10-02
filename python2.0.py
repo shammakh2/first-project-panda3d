55f08305a7464e888e549ed791e47b7789fd2687
b9003599 <b9003599@newcastle.ac.uk>
2019-10-02 11:04:02 +0100

    3d pamda add

--- python2.0.py
+++ python2.0.py
@@ -1,9 +1,37 @@
+
+from math import pi, sin, cos
+
 from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+from direct.actor.Actor import Actor
 
 class MyApp(ShowBase):
 
     def __init__(self):
         ShowBase.__init__(self)
 
-app = MyApp()
-app.run()
+        self.scene = self.loader.loadModel("models/environment")
+        self.scene.reparentTo(self.render)
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, 0)
+
+        ##adding spin camera
+        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+
+        #Add 3d panda
+        self.pandaActor= Actor("models/panda-model", {"walk": models/panda-walk4})
+        self.pandaActor.setScale(0.005, 0.005, 0.005)
+        self.pandaActor.reparentTo(self.render)
+
+        #Loop panda animation
+        self.pandaActor.loop("walk")
+
+        #Move camera
+        def spinCameraTask(self, task):
+            angleDegrees = task.time * 6.0
+            angleRadians = angleDegrees * (pi / 180.0)
+            self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
+            self.camera.setHpr(angleDegrees, 0, 0)
+            return Task.cont
+
+MyApp().run()
