bffe59011173fca3439f2d9bc7658f01050d51da
b9003599 <b9003599@newcastle.ac.uk>
2019-10-02 11:31:02 +0100

    3d pamda add test 2

--- python2.0.py
+++ python2.0.py
@@ -5,8 +5,7 @@ from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
 
-class MyApp(ShowBase):
-
+class MyAp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
@@ -15,23 +14,23 @@ class MyApp(ShowBase):
         self.scene.setScale(0.25, 0.25, 0.25)
         self.scene.setPos(-8, 42, 0)
 
-        ##adding spin camera
+        # Add the spinCameraTask procedure to the task manager.
         self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
 
-        #Add 3d panda
-        self.pandaActor= Actor("models/panda-model", {"walk": models/panda-walk4})
+        # Add 3d panda
+        self.pandaActor= Actor("models/panda-model", {"walk": "models/panda-walk4"})
         self.pandaActor.setScale(0.005, 0.005, 0.005)
         self.pandaActor.reparentTo(self.render)
 
-        #Loop panda animation
+        # Loop panda animation
         self.pandaActor.loop("walk")
 
-        #Move camera
-        def spinCameraTask(self, task):
+        # move camera.
+        def SpinCameraTask(self, task):
             angleDegrees = task.time * 6.0
             angleRadians = angleDegrees * (pi / 180.0)
             self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
             self.camera.setHpr(angleDegrees, 0, 0)
             return Task.cont
 
-MyApp().run()
+MyAp().run()
