cf837a2daf2850acb37044ed91a2fdcdb051549f
b9003599 <b9003599@newcastle.ac.uk>
2019-10-03 10:38:38 +0100

    Complete spin panda file

--- python2.0.py
+++ python2.0.py
@@ -1,3 +1,39 @@
+# from math import pi, sin, cos
+#
+# from direct.showbase.ShowBase import ShowBase
+# from direct.task import Task
+# from direct.actor.Actor import Actor
+#
+# class MyAp(ShowBase):
+#     def __init__(self):
+#         ShowBase.__init__(self)
+#
+#
+#         self.scene = self.loader.loadModel("models/environment")
+#         self.scene.reparentTo(self.render)
+#         self.scene.setScale(0.25, 0.25, 0.25)
+#         self.scene.setPos(-8, 42, 0)
+#
+#         # Add the spinCameraTask procedure to the task manager.
+#         self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+#
+#         # Add 3d panda
+#         self.pandaActor= Actor("models/panda-model",{"walk": "models/panda-walk4"})
+#         self.pandaActor.setScale(0.005, 0.005, 0.005)
+#         self.pandaActor.reparentTo(self.render)
+#         # Loop panda animation
+#         self.pandaActor.loop("walk")
+#
+#         # move camera.
+#         def spinCameraTask(self, task):
+#             angleDegrees = task.time * 6.0
+#             angleRadians = angleDegrees * (pi / 180.0)
+#             self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
+#             self.camera.setHpr(angleDegrees, 0, 0)
+#             return Task.cont
+#
+# MyAp().run()
+
 
 from math import pi, sin, cos
 
@@ -9,28 +45,32 @@ class MyAp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
+        # Load the environment model.
         self.scene = self.loader.loadModel("models/environment")
+        # Reparent the model to render.
         self.scene.reparentTo(self.render)
+        # Apply scale and position transforms on the model.
         self.scene.setScale(0.25, 0.25, 0.25)
         self.scene.setPos(-8, 42, 0)
 
         # Add the spinCameraTask procedure to the task manager.
         self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
 
-        # Add 3d panda
-        self.pandaActor= Actor("models/panda-model", {"walk": "models/panda-walk4"})
+        # Load and transform the panda actor.
+        self.pandaActor = Actor("models/panda-model",
+                                {"walk": "models/panda-walk4"})
         self.pandaActor.setScale(0.005, 0.005, 0.005)
         self.pandaActor.reparentTo(self.render)
-
-        # Loop panda animation
+        # Loop its animation.
         self.pandaActor.loop("walk")
 
-        # move camera.
-        def SpinCameraTask(self, task):
-            angleDegrees = task.time * 6.0
-            angleRadians = angleDegrees * (pi / 180.0)
-            self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
-            self.camera.setHpr(angleDegrees, 0, 0)
-            return Task.cont
+    # Define a procedure to move the camera.
+    def spinCameraTask(self, task):
+        angleDegrees = task.time * 6.0
+        angleRadians = angleDegrees * (pi / 180.0)
+        self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
+        self.camera.setHpr(angleDegrees, 0, 0)
+        return Task.cont
 
-MyAp.run()
+MyAp().run()
+
